d44b91edbba8062994ba5f9bff2a4249717870b1
Stefano Mandelli <linuxfree2@gmail.com>
2016-03-28 01:49:39 +0200

    Commit iniziale - Aggiunta di tutti i file al progetto

---
+++ footer.php
@@ -0,0 +1,20 @@
+<div class="credits section-inner">
+			
+	<p>&copy; <?php echo date('Y'); ?> <a href="http://linuxfree.ddns.net/lisa">LISA BUSTERNA ATELIER D'ARTE</a></p>
+	<div>
+		<center>
+			<div class="social-box">
+			<a href="https://www.facebook.com/BusternaAtelierArte" class="social-media-icon smi-facebook" target="_blanck"><i style="color: black;" class="fa fa-facebook"></i></a>
+			<a href="https://plus.google.com/u/0/100888601652328770376" class="social-media-icon smi-googlep" target="_blanck"><i style="color: black;" class="fa fa-google-plus"></i></a>
+			<a href="http://www.gigarte.com/lisa-busterna/home" class="social-media-icon smi-gigarte" target="_blanck"><i style="color: black;" class="fa fa-paint-brush"></i></a>
+			</div>
+		</center>
+	</div>
+	<div class="clear"></div>
+	
+</div> <!-- /credits -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
